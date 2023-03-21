e72bee0b14b521a32b807a340769af62ed33b005
Cícero Medeiros <cicero.medeiros@codeminer42.com>
2023-03-21 16:52:52 -0300

    feat: Adds Header

---
+++ src/ui/theme/Header.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  font-family: "Red Hat Diplay";
+  font-weight: 900;
+  text-align: center;
+  color: var(--white);
+  height: 64px;
+  background: #262626;
+  grid-column: span 2;
+`;
+
+export default Header;
