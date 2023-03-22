dbc42a3113ec4d684f542bb0533650f70a0868c8
gabrielcicero45 <42891342+gabrielcicero45@users.noreply.github.com>
2023-03-22 10:04:04 -0300

    Update src/ui/components/Content.js
    Co-authored-by: Kátia Bernardes <89419522+kabernardes@users.noreply.github.com>

--- src/ui/components/Content.js
+++ src/ui/components/Content.js
@@ -37,7 +37,7 @@ const Content = () => {
             </Button>
             <QuizesList quizes={quizes} />
           </TextBanner>
-          <Banner></Banner>
+          <Banner />
         </>
       )}
     </Wrapper>
