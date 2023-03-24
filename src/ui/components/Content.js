291cbbfc8702df081a939746e298dfe798add0e3
Cícero Medeiros <cicero.medeiros@codeminer42.com>
2023-03-24 13:49:15 -0300

    feat: Adds Switch theme component

--- src/ui/components/Content.js
+++ src/ui/components/Content.js
@@ -1,4 +1,4 @@
-import { Button, useColorMode } from "theme-ui";
+import { useColorMode } from "theme-ui";
 import { Header, HeaderText } from "../styles/Header";
 import { quizCreationEnded } from "../../state/actions/quizActions";
 import { useSelector, useDispatch } from "react-redux";
@@ -8,28 +8,36 @@ import QuizesList from "./QuizesList";
 import QuizForm from "./QuizForm";
 import React from "react";
 import Wrapper from "../styles/Wrapper";
-
+import Switch from "../styles/Switch";
 const Content = () => {
-  const viewState = useSelector(
-    (state) => state.quizReducer.viewState
-  );
+  const viewState = useSelector((state) => state.quizReducer.viewState);
   const dispatch = useDispatch();
   const [colorMode, setColorMode] = useColorMode();
 
   return (
     <Wrapper>
       <Header>
-        <Notification/>
+        <Notification />
         <HeaderText onClick={() => dispatch(quizCreationEnded())}>
           Noises & Quizzes
         </HeaderText>
-        <Button onClick={(e) => {
-          setColorMode(colorMode === 'default' ? 'dark' : 'default')
-        }}>Change color</Button>
+        <HeaderText>
+          Switch theme: 
+          <Switch>
+            <input
+              type="checkbox"
+              role="switch"
+              onClick={(e) => {
+                setColorMode(colorMode === "default" ? "dark" : "default");
+              }}
+            />
+            <span></span>
+          </Switch>
+        </HeaderText>
       </Header>
-      {viewState === "Home" && <Home/>}
+      {viewState === "Home" && <Home />}
       {viewState === "Quiz Form" && <QuizForm />}
-      {viewState === "Quizzes List" && <QuizesList/>}
+      {viewState === "Quizzes List" && <QuizesList />}
     </Wrapper>
   );
 };
